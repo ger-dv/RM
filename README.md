3f8ab9608f169cbd41e3cf8e87dca13753ac5302
Germán Díaz <ger.dv@outlook.com>
2023-07-10 13:46:15 -0600

    app

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# RM
-Simple React App
+# Rick & Morty Simple React App
+
+This is a simple React App that consumes [The Rick and Morty API](https://rickandmortyapi.com/) to display Rick and Morty characters info.
+
+[Live Demo](https://reliable-dango-23e483.netlify.app/)
---
+++ src/App.js
@@ -0,0 +1,32 @@
+import imageRM from './img/rick-morty.png';
+import './App.css';
+import { useState } from 'react';
+import Characters from './components/Characters';
+
+function App() {
+  const [characters, setCharacters] = useState(null);
+
+  const reqApi = async () => {
+    // console.log("Click");
+    const api = await fetch('https://rickandmortyapi.com/api/character');
+    const characterApi = await api.json();
+    setCharacters(characterApi.results);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title">Rick & Morty</h1>
+        {characters ? ( <Characters characters = {characters} setCharacters={setCharacters} /> ) : 
+        (
+          <>
+            <img src={imageRM} alt='Rick & Morty' className='img-home'/>
+            <button onClick={reqApi} className='btn-search'>Buscar personajes</button>
+          </>
+        )} 
+      </header>
+    </div>
+  );
+}
+
+export default App;
